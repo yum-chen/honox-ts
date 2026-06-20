8178a44ffdb07e68c7fd53a72c94a7b6bd9c0681
Yumin Chen <yumin@chen.so>
2026-06-20 22:53:37 +0100

    Setup @hono/vite-ssg and Cloudflare Pages deployment script
    - Replaced `@hono/vite-build/cloudflare-workers` with `@hono/vite-ssg`.
    - Updated `vite.config.ts` to support SSG and split it to `mainConfig` and `clientConfig` for client-side islands.
    - Modified `wrangler.jsonc` to remove the worker entry point, aligning with static site deployment.
    - Updated `deploy` script in `package.json`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
-import build from "@hono/vite-build/cloudflare-workers";
 import adapter from "@hono/vite-dev-server/cloudflare";
+import ssg from "@hono/vite-ssg";
 import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+const config = defineConfig(({ mode }) =>
+	mode === "client" ? clientConfig : mainConfig,
+);
+
+const mainConfig = {
+	build: {
+		emptyOutDir: false,
+	},
 	plugins: [
 		honox({
 			devServer: { adapter },
+		}),
+		tailwindcss(),
+		ssg({ entry: "app/server.ts" }),
+	],
+};
+
+const clientConfig = {
+	plugins: [
+		honox({
 			client: { input: ["/app/client.ts", "/app/style.css"] },
 		}),
 		tailwindcss(),
-		build(),
 	],
-});
+};
+
+export default config;
