d76b81a17a745f922d374c0b60cafd40455990ed
Yumin Chen <yumin@chen.so>
2026-06-25 10:24:54 +0100

    Set up Park UI
    - Run `bunx @park-ui/cli init`

--- panda.config.ts
+++ panda.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@pandacss/dev";
+import * as theme from "./app/theme";
 
 export default defineConfig({
 	// Whether to use css reset
@@ -11,10 +12,26 @@ export default defineConfig({
 	exclude: [],
 
 	// Useful for theme customization
-	theme: {
-		extend: {},
-	},
+	theme: { extend: { ...theme.config } },
 
 	// The output directory for your css system
 	outdir: "styled-system",
+
+	plugins: [
+		{
+			name: "Remove Panda Preset Colors",
+			hooks: {
+				"preset:resolved": ({ utils, preset, name }) =>
+					name === "@pandacss/preset-panda"
+						? utils.omit(preset, [
+								"theme.tokens.colors",
+								"theme.semanticTokens.colors",
+							])
+						: preset,
+			},
+		},
+	],
+
+	globalCss: theme.globalCss,
+	conditions: theme.conditions,
 });
