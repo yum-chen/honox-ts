844d2510cea411b61c6516883d6ad3b6057d2321
Yumin Chen <yumin@chen.so>
2026-06-27 07:09:44 +0100

    fix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,7 +13,6 @@ export default createRoute((c) => {
 	const name = c.req.query("name") ?? "Hono";
 	return c.render(
 		<div class={css({ py: "8", textAlign: "center" })}>
-			<title>{name}</title>
 			<Heading as="h1" class={css({ fontSize: "3xl", fontWeight: "bold" })}>
 				Hello, {name}!
 			</Heading>
