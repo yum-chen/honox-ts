6fbad6c9a6466696d2356d2fcf6b31208026f07a
Yumin Chen <yumin@chen.so>
2026-07-03 22:32:35 +0100

    Fix _renderer.tsx

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -11,7 +11,7 @@ export default jsxRenderer(({ children }) => {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="icon" href="/favicon.ico" />
 				<Link href="/app/style.css" rel="stylesheet" />
-				<Script src="/app/client.ts" async />
+				<Script src="/app/client.ts" async prod={true}/>
 			</head>
 			<body>{children}</body>
 		</html>
